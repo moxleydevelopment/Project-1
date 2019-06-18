0a762afaf620a6a8241b74232009d27f3c8d0349
Donya Moxley <moxleydevelopment@gmail.com>
2019-06-18 01:06:52 -0400

    Created title, decsription, rules, and added wireframe to project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 <h1>SIMON SAYS<h1>
+<p>This is a recreation of a child memory game, where the computer plays a sequence of light patters that the user must mimic in order to proceed to the next round and win the game. This recreation is a web based form of that same memory game throught the usage of HTML, CSS and Javascript.
+<p>
+
+<h4>Directions<h4>
+<p>The game will begin when the user clicks on the start button, and the sequence will start. The user will alow the light sequence to play first, then when sequene stops the user should click on the circles with pointer in the order they were lit up. After the correct sequence is played back the round counter will increase by one and the next sequene of lights will being to play.Should the incorrect sequence occur the game will buzz and display game over.
+<p>
+
+<h4>Development<h4>
+
+<h6>Wireframe<h6>
+
+![alt text](images/Screen Shot 2019-06-12 at 11.22.09 AM.png "Beginning mock up of project")
+
+<p>In the beggining the desired look was an exact replica of the tangible game, will all real life functionality to create a more varient experience for the user<p>
--- js/jquery.js
+++ js/jquery.js
@@ -10,15 +10,15 @@ let displayRound = $('h3')
 let sequence = []
 let userSequence = []
 let num = 0
-let isOn 
+let isOn
 let round = 1
-let isComputer 
-let isWinner 
-let gameLoop 
+let isComputer
+let isWinner
+let gameLoop
 let isCorrect
 let counter = 0
 
-displayRound.text('0'+ round)
+displayRound.text('0' + round)
 
 
 
@@ -26,45 +26,45 @@ displayRound.text('0'+ round)
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min; 
-  }
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
-  $.fn.getLightSequence = function(){
-      for( let i = 0; i < 10; i++){
-        num = getRandomInt(0 , 4)
+$.fn.getLightSequence = function () {
+    for (let i = 0; i < 10; i++) {
+        num = getRandomInt(0, 4)
         sequence.push(num)
-        
-        
-      }
-  }
 
-  
 
-$.fn.gameSequence = function(){
-  isOn = false
-  
- if (counter == round){
-        clearInterval(gameLoop) 
-        isComputer = false 
+    }
+}
+
+
+
+$.fn.gameSequence = function () {
+    isOn = false
+
+    if (counter == round) {
+        clearInterval(gameLoop)
+        isComputer = false
         $.fn.colorReset()
         isOn = true
-        
+
     }
 
-    if(isComputer){
-        $.fn.colorReset() 
-        setTimeout(function() {
-            if(sequence[counter] == 0){
+    if (isComputer) {
+        $.fn.colorReset()
+        setTimeout(function () {
+            if (sequence[counter] == 0) {
                 $.fn.zero()
             }
-            if(sequence[counter] == 1){
+            if (sequence[counter] == 1) {
                 $.fn.one()
             }
-            if(sequence[counter] == 2){
-               $.fn.two()
+            if (sequence[counter] == 2) {
+                $.fn.two()
             }
-            if(sequence[counter] == 3){
-               $.fn.three()
+            if (sequence[counter] == 3) {
+                $.fn.three()
             }
             counter++
 
@@ -72,42 +72,42 @@ $.fn.gameSequence = function(){
         }, 200)
 
     }
-    
+
 }
 
 
 
-$.fn.newGame = function (){
+$.fn.newGame = function () {
 
-    isWinner = false 
+    isWinner = false
     isComputer = true
     isCorrect = true
     $.fn.getLightSequence()
-    gameLoop = setInterval(function() {$.fn.gameSequence()}, 800)
+    gameLoop = setInterval(function () { $.fn.gameSequence() }, 800)
 }
 
-$.fn.zero = function(){
+$.fn.zero = function () {
     document.getElementById('audioZero').play()
     squareZero.css("background-color", "lightgreen")
     squareZero.addClass('animate pulse')
 }
 
-$.fn.one = function(){
-     
+$.fn.one = function () {
+
     document.getElementById('audioOne').play()
     squareOne.css("background-color", "red")
     squareOne.addClass('animate pulse')
 }
 
-$.fn.two = function(){
-   
+$.fn.two = function () {
+
     document.getElementById('audioTwo').play()
     squareTwo.css("background-color", "lightblue")
     squareTwo.addClass('animate pulse')
 }
 
-$.fn.three = function(){
-   
+$.fn.three = function () {
+
     document.getElementById('audioThree').play()
     squareThree.css("background-color", "yellow")
     squareThree.addClass('animate pulse')
@@ -116,41 +116,41 @@ $.fn.three = function(){
 
 
 
-$.fn.clickIndicator = function(callback){
-   userSequence.push(Number(this.attr('id')))
-  
-    
+$.fn.clickIndicator = function (callback) {
+    userSequence.push(Number(this.attr('id')))
+
+
 
 
 }
 
-$.fn.checkSequence = function(){
+$.fn.checkSequence = function () {
     let check = (userSequence.length - 1)
-    console.log( isCorrect)
-    console.log( userSequence.length)
-   
-    if (userSequence[check] !== sequence[check]){
-       isCorrect = false
+    console.log(isCorrect)
+    console.log(userSequence.length)
+
+    if (userSequence[check] !== sequence[check]) {
+        isCorrect = false
     }
-     if((userSequence.length === 2) && (isCorrect == true)){
+    if ((userSequence.length === 2) && (isCorrect == true)) {
         displayRound.text('PLAYER WON!!')
         isWinner = true
     }
-    if(isCorrect === false){
-        
+    if (isCorrect === false) {
+
         document.getElementById('game-over').play()
         displayRound.text('GAME OVER')
-        setTimeout(() =>{
+        setTimeout(() => {
             $.fn.colorReset()
 
         })
     }
 
-    if (round == userSequence.length){
-        if(isCorrect && !isWinner){
-            round++ 
-            if(round < 10){
-                displayRound.text('0'+ round)
+    if (round == userSequence.length) {
+        if (isCorrect && !isWinner) {
+            round++
+            if (round < 10) {
+                displayRound.text('0' + round)
             }
             else {
                 displayRound.text(round)
@@ -158,74 +158,74 @@ $.fn.checkSequence = function(){
             userSequence = []
             isComputer = true
             counter = 0
-            gameLoop = setInterval(function() {$.fn.gameSequence()}, 800)
+            gameLoop = setInterval(function () { $.fn.gameSequence() }, 800)
         }
     }
 
-   
+
 }
 
 
 
-$.fn.colorReset = function() {
-    squareZero.css("background-color", "darkgreen") 
+$.fn.colorReset = function () {
+    squareZero.css("background-color", "darkgreen")
     squareOne.css("background-color", "darkred")
     squareTwo.css("background-color", "darkblue")
     squareThree.css("background-color", "goldenrod")
 }
 
-squareZero.on('click', function() {
-    if(isOn == true){
-    squareZero.clickIndicator() 
-    $.fn.checkSequence() 
-    $.fn.zero()
+squareZero.on('click', function () {
+    if (isOn == true) {
+        squareZero.clickIndicator()
+        $.fn.checkSequence()
+        $.fn.zero()
     }
-    if(!isWinner){
-        setTimeout(function() {
-         $.fn.colorReset(), 1600
-        })  
+    if (!isWinner) {
+        setTimeout(function () {
+            $.fn.colorReset(), 1600
+        })
     }
 })
-squareOne.on('click', function() {
-    if(isOn == true){
-        squareOne.clickIndicator() 
-        $.fn.checkSequence() 
+squareOne.on('click', function () {
+    if (isOn == true) {
+        squareOne.clickIndicator()
+        $.fn.checkSequence()
         $.fn.one()
-        }
-        if(!isWinner){
-            setTimeout(function() {
-             $.fn.colorReset(), 1600
-            })  
-        }
-    
+    }
+    if (!isWinner) {
+        setTimeout(function () {
+            $.fn.colorReset(), 1600
+        })
+    }
+
 })
-squareTwo.on('click', function() {
-    if(isOn == true){
-        squareTwo.clickIndicator() 
-        $.fn.checkSequence() 
+squareTwo.on('click', function () {
+    if (isOn == true) {
+        squareTwo.clickIndicator()
+        $.fn.checkSequence()
         $.fn.two()
-        }
-        if(!isWinner){
-            setTimeout(function() {
-             $.fn.colorReset(), 1600
-            })  
-        }
+    }
+    if (!isWinner) {
+        setTimeout(function () {
+            $.fn.colorReset(), 1600
+        })
+    }
 })
-squareThree.on('click', function() {
-    if(isOn == true){
-        squareThree.clickIndicator() 
-        $.fn.checkSequence() 
+squareThree.on('click', function () {
+    if (isOn == true) {
+        squareThree.clickIndicator()
+        $.fn.checkSequence()
         $.fn.three()
-        }
-        if(!isWinner){
-            setTimeout(function() {
-             $.fn.colorReset(), 1600
-            })  
-        }
+    }
+    if (!isWinner) {
+        setTimeout(function () {
+            $.fn.colorReset(), 1600
+        })
+    }
 })
 
 
-startBtn.on('click', function(){
+startBtn.on('click', function () {
     $.fn.newGame()
 })
 
